9b367895b422039b6b926c7ce21230c444640603
islamhany <islamhany2268@gmail.com>
2021-05-26 21:04:59 +0200

    Auth service implementation completed

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,8 @@ import express from 'express';
 const router = express.Router();
 
 router.post('/api/users/signout', (req, res) => {
-    res.send("Hi there");
+    req.session = null; //tell the browser to dump the cookie
+    res.send({});
 });
 
 export {router as signoutRouter};
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,10 @@
 import express, {Request, Response} from 'express';
-import {body, validationResult} from 'express-validator';
+import {body} from 'express-validator';
+import jwt from 'jsonwebtoken';
+
 import {User} from '../models/user';
-import {RequestValidationError} from '../errors/request-validation-error';
 import {BadRequestError} from '../errors/bad-request-error';
+import {validateRequest} from '../middlewares/validate-request';
 
 const router = express.Router();
 
@@ -14,12 +16,9 @@ router.post('/api/users/signup', [
         .trim()
         .isLength({min: 4, max: 20})
         .withMessage("Password must be between 4 and 20 characters")
-], async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if(!errors.isEmpty()){
-        throw new RequestValidationError(errors.array());
-    }
+], 
+validateRequest,
+async (req: Request, res: Response) => {
 
     const {email, password} = req.body;
 
@@ -32,7 +31,16 @@ router.post('/api/users/signup', [
     const user = User.build({email, password});
     await user.save();
 
-    res.status(201).send({user});
+    const userJwt = jwt.sign({
+        id: user.id,
+        email: user.email
+    }, process.env.JWT_KEY!);
+
+    req.session = {
+        jwt: userJwt
+    };
+
+    res.status(201).send(user);
 });
 
 export {router as signupRouter};
